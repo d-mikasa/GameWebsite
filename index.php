f15a265456aee6b2aab91d2043e002e4231a05ab
mika <d.mikasa@ebacorp.jp>
2020-09-04 06:08:36 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Title</title>
+	<meta name="description" content="Title">
+	<link rel="icon" href="images/favicon.png" type="image/png">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<!--fot-->
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+
+
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>HOME</li>
+				<li>FPS</li>
+				<li>MMO</li>
+				<li>MOVA</li>
+			</ul>
+
+		</nav>
+		<div class="background-color">
+			<div class="title-text"><p>DO THE</p><p class = "enjoy">ENJOY</p></div>
+		</div>
+	</header>
+	<main class="content-folder">
+
+
+		<section class="profire">PROFIRE<div class="profire-text"><!--Startプロフィール-->
+				<table>
+					<tr>
+						<th>NAME</th>
+						<td>Mikasa Daichi</td>
+					</tr>
+
+					<tr>
+						<th>HAS BORNE</th>
+						<td>1997/09/07</td>
+					</tr>
+					<tr>
+						<th rowspan="3">Like</th>
+						<td>Riot Games(LoL,Valorant)</td>
+					</tr>
+					<tr>
+						<td>LittleBusters</td>
+					</tr>
+					<tr>
+						<td>Hypnosys mic</td>
+					</tr>
+
+				</table>
+
+			</div>
+		</section><!--Endプロフィール-->
+
+		<section class="profire">HOBY<div class="profire-text"><!--Start作品-->
+			<ul>
+				<li>
+					Ilast
+				</li>
+				<li>
+					Book
+				</li>
+				<li>
+					Ather
+				</li>
+			</ul>
+
+	</section><!--End作品-->
+
+	<section class="profire">BLOG<div class="profire-text"><!--Startブログ-->
+		<table>
+			<tl>
+				<td> Title</td>
+				<td>subtitle</td>
+			</tl>
+		</table>
+	</div>
+</section><!--Endブログ-->
+
+	</main>
+</body>
+</html>
